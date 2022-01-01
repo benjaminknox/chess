9e784b2143fed996ecb3c74c7fcc4c0aee038eb0
Benjamin Knox <bknox.contact@gmail.com>
2022-01-01 15:46:29 -0500

    Fixes Game test - added store context

--- frontend/src/@pages/games/Game.spec.tsx
+++ frontend/src/@pages/games/Game.spec.tsx
@@ -1,14 +1,22 @@
 import { Game } from './Game'
 import * as React from 'react'
+import createStore from '@store'
 import { mount } from '@cypress/react'
+import { StoreContext } from 'storeon/react'
 import { MemoryRouter } from 'react-router-dom'
+import { fakeIdentity } from '@testUtils/fakeIdentity'
 
 describe('Game', () => {
   it('should show game board', () => {
     cy.viewport(800, 800)
+    const store = createStore()
+    store.dispatch('auth/setIdentity', fakeIdentity)
+
     mount(
       <MemoryRouter initialEntries={['/game/test-game-id']}>
-        <Game />
+        <StoreContext.Provider value={store}>
+          <Game />
+        </StoreContext.Provider>
       </MemoryRouter>
     )
     cy.get('[data-square] [draggable]').should('have.length', 32)
